c2d7c5b9a2f09871494416079ec0c975d2f55fed
batista <batistaoliveiradev@gmail.com>
2021-12-02 18:29:34 -0300

    Add project files.

---
+++ EFCore.WebAPI/Data/HeroiContext.cs
@@ -0,0 +1,35 @@
+﻿using EFCore.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.WebAPI.Data
+{
+    public class HeroiContext: DbContext
+    {
+        public DbSet<Heroi> Herois { get; set; }
+        public DbSet<Batalha> Batalhas { get; set; }
+        public DbSet<Arma> Armas { get; set; }
+        public DbSet<HeroiBatalha> HeroisBatalhas { get; set; }
+        public DbSet<IdentidadeSecreta> IdentidadesSecretas { get; set; }
+
+        //string de conexão
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        { 
+            optionsBuilder.UseSqlServer(@"Server=LAPTOP-JIFGMMN2\SQLEXPRESS;Database=HeroAPP;User=sa;Password=sqlserver;") ;
+
+        }
+
+        //define BatalhaId e HeroiId como chaves primárias para a tabela HeroiBatalha
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HeroiBatalha>(entity =>
+            {
+                entity.HasKey(e => new { e.BatalhaId, e.HeroiId });
+            }
+            );
+        }
+    }
+}
